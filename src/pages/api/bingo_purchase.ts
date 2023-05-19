744ef0845993eec357ccdc841d933af7ceb4bc37
Pjijin <pjijin1@gmail.com>
2023-05-19 10:33:22 +0530

    🐛 Fix: added setup.sh

--- src/pages/api/bingo_purchase.ts
+++ src/pages/api/bingo_purchase.ts
@@ -5,6 +5,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     // things to do after the purchase is completed?
 
+    // lets call from the graph api req
+
     // 1. Check if the tx is confirmed.
     // 2. Get the token id minted. make sure the tokenid does not exist on db along with the campaign id
     // 3. From the campaigns generate 24 tasks for user accounts
