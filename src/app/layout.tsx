aa116d4ea8a53331594834b6d45c2ec7119d823f
Pjijin <pjijin1@gmail.com>
2023-05-30 12:03:03 +0530

    🐛 Fix: updated meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,21 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-	title: 'Bingo',
+	title: 'LFBingo',
 	description: 'Bingo ',
+	icons: {
+		icon: '/assets/favicon.ico',
+	},
+	'twitter:card': 'summary_large_image',
+	'twitter:site': '@beyondclub_xyz',
+	'twitter:title': 'LFBingo',
+	'twitter:description': 'Bingo',
+	'twitter:image': 'https://www.lfbingo.xyz/assets/og.png',
+	'og:url': 'https://www.lfbingo.xyz',
+	'og:type': 'website',
+	'og:title': 'LFBingo',
+	'og:description': 'Bingo',
+	'og:image': 'https://www.lfbingo.xyz/assets/og.png',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
