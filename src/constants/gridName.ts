7567fc0547d23c482e8b87f459c01a0cb4b2155e
Pjijin <pjijin1@gmail.com>
2023-06-03 13:16:38 +0530

    🐛 Fix: updated tasks

--- src/constants/gridName.ts
+++ src/constants/gridName.ts
@@ -1,13 +1,18 @@
 export const gridName = {
     uniswap_swap: 'swap \n [N] times on \n Uniswap',
     uniswap_liquidity: 'Provide \n Liquidity on \n Uniswap',
+    sushiswap_liquidity: 'Provide \n Liquidity on \n Sushiswap',
+    curve_liquidity: 'Provide \n Liquidity on \n Curve',
+    rocketpool_stake: 'Stake on \n RocketPool',
 
     "1inch_swap": 'swap \n [N] times on \n 1inch',
     curve_swap: 'swap \n [N] times on \n Curve',
     sushiswap_swap: 'swap \n [N] times on \n Sushiswap',
+    snapshot_vote: 'vote on \n Snapshot',
 
     aave_lend: 'lend on \n Aave',
     compound_lend: 'lend on \n Compound',
+    yearn_deposit: 'lend on \n Yearn',
 
 
     "poap": 'Hold \n [N] POAP',
@@ -24,6 +29,7 @@ export const gridName = {
     eth_balance: 'Hold \n [N] ETH',
     token_balance_0x6b175474e89094c44da98b954eedeac495271d0f: 'Hold \n [N] DAI',
     token_balance_0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48: 'Hold \n [N] USDC',
+    token_balance_0xdAC17F958D2ee523a2206206994597C13D831ec7: 'Hold \n [N] USDT',
 
     tx_history_polygon: '1tx on \n Polygon',
     tx_history_optimism: '1tx on \n Optimism',
